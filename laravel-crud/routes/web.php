9dfc4b3391889a8afe7a0c7baed0a1ed20582412
unknown <frenotama9@gmail.com>
2024-11-19 07:34:32 +0700

    second commit

--- laravel-crud/routes/web.php
+++ laravel-crud/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\NewsController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('news', [NewsController::class, "index"]);
